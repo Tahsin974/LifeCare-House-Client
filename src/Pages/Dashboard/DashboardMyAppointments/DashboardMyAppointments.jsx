2ed59b2043a4bec6918bb78a91c1393569202382
Tahsin974 <tahsinislam974@gmail.com>
2025-02-20 16:00:51 +0600

    setep jwt and create apis and verify token task completed

--- src/Pages/Dashboard/DashboardMyAppointments/DashboardMyAppointments.jsx
+++ src/Pages/Dashboard/DashboardMyAppointments/DashboardMyAppointments.jsx
@@ -22,35 +22,47 @@ const DashboardMyAppointments = () => {
         </div>
       ) : (
         <div>
-          <div>
+          <div className="pb-10">
             <h1 className="text-3xl font-bold text-left">
               My Appointments : {myAppointments.length}
             </h1>
           </div>
-          <div className="overflow-x-auto mt-5 rounded-lg">
-            <table className="table">
-              {/* head */}
-              <thead className="bg-[#E6E6E6] text-black">
-                <tr>
-                  <th>#</th>
-                  <th>NAME</th>
-                  <th>DATE</th>
-                  <th>TIME</th>
-                  <th>TREATMENT</th>
-                  <th>PAYMENT</th>
-                </tr>
-              </thead>
-              <tbody className="bg-white">
-                {myAppointments.map((appointment, index) => (
-                  <TableRow
-                    key={appointment._id}
-                    index={index}
-                    appointment={appointment}
-                  />
-                ))}
-              </tbody>
-            </table>
-          </div>
+          {!myAppointments.length ? (
+            <div className="hero bg-white min-h-screen">
+              <div className="hero-content text-center">
+                <div>
+                  <h1 className="lg:text-5xl md:text-2xl text-xl font-bold text-slate-400">
+                    You Do Not Have <br /> Any Appointments
+                  </h1>
+                </div>
+              </div>
+            </div>
+          ) : (
+            <div className="overflow-x-auto mt-5 rounded-lg">
+              <table className="table">
+                {/* head */}
+                <thead className="bg-[#E6E6E6] text-black">
+                  <tr>
+                    <th>#</th>
+                    <th>NAME</th>
+                    <th>DATE</th>
+                    <th>TIME</th>
+                    <th>TREATMENT</th>
+                    <th>PAYMENT</th>
+                  </tr>
+                </thead>
+                <tbody className="bg-white">
+                  {myAppointments.map((appointment, index) => (
+                    <TableRow
+                      key={appointment._id}
+                      index={index}
+                      appointment={appointment}
+                    />
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          )}
         </div>
       )}
     </div>
