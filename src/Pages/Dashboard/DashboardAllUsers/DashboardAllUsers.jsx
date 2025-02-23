d7a477582a042a68b9e56c67d168f3891c0c6f64
Tahsin974 <tahsinislam974@gmail.com>
2025-02-23 14:53:03 +0600

    axios interceptor done

--- src/Pages/Dashboard/DashboardAllUsers/DashboardAllUsers.jsx
+++ src/Pages/Dashboard/DashboardAllUsers/DashboardAllUsers.jsx
@@ -6,7 +6,7 @@ const DashboardAllUsers = () => {
   const axiosSecure = useAxiosSecure();
   const {
     data: allUsers = [],
-
+    isPending,
     refetch,
   } = useQuery({
     queryKey: ["all-users"],
@@ -18,46 +18,54 @@ const DashboardAllUsers = () => {
   return (
     <div className=" bg-[#F1F5F9] min-h-screen p-10">
       <div>
-        <div className="pb-10">
-          <h1 className="text-3xl font-bold text-left">
-            All Users : {allUsers.length}
-          </h1>
-        </div>
-        {!allUsers.length ? (
-          <div className="hero bg-white min-h-screen">
-            <div className="hero-content text-center">
-              <div>
-                <h1 className="lg:text-5xl md:text-2xl text-xl font-bold text-slate-400">
-                  You Do Not Have <br /> Any Appointments
-                </h1>
-              </div>
-            </div>
+        {isPending ? (
+          <div className="min-h-screen flex justify-center items-center">
+            <span className="loading loading-ring loading-lg"></span>
           </div>
         ) : (
-          <div className="overflow-x-auto mt-5 rounded-lg">
-            <table className="table text-center">
-              {/* head */}
-              <thead className="bg-[#E6E6E6] text-black">
-                <tr>
-                  <th>#</th>
-                  <th>NAME</th>
-                  <th>EMAIL</th>
-                  <th>JOB</th>
-                  <th>ACTION</th>
-                </tr>
-              </thead>
-              <tbody className="bg-white">
-                {allUsers.map((user, index) => (
-                  <AllUsersTableRow
-                    key={user._id}
-                    index={index}
-                    user={user}
-                    refetch={refetch}
-                  />
-                ))}
-              </tbody>
-            </table>
-          </div>
+          <>
+            <div className="pb-10">
+              <h1 className="text-3xl font-bold text-left">
+                All Users : {allUsers.length}
+              </h1>
+            </div>
+            {!allUsers.length ? (
+              <div className="hero bg-white min-h-screen">
+                <div className="hero-content text-center">
+                  <div>
+                    <h1 className="lg:text-5xl md:text-2xl text-xl font-bold text-slate-400">
+                      You Do Not Have <br /> Any Appointments
+                    </h1>
+                  </div>
+                </div>
+              </div>
+            ) : (
+              <div className="overflow-x-auto mt-5 rounded-lg">
+                <table className="table text-center">
+                  {/* head */}
+                  <thead className="bg-[#E6E6E6] text-black">
+                    <tr>
+                      <th>#</th>
+                      <th>NAME</th>
+                      <th>EMAIL</th>
+                      <th>JOB</th>
+                      <th>ACTION</th>
+                    </tr>
+                  </thead>
+                  <tbody className="bg-white">
+                    {allUsers.map((user, index) => (
+                      <AllUsersTableRow
+                        key={user._id}
+                        index={index}
+                        user={user}
+                        refetch={refetch}
+                      />
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            )}
+          </>
         )}
       </div>
     </div>
--- src/Pages/Dashboard/DashboardMyAppointments/DashboardMyAppointments.jsx
+++ src/Pages/Dashboard/DashboardMyAppointments/DashboardMyAppointments.jsx
@@ -6,7 +6,7 @@ import AppointmentsTableRow from "./AppointmentsTableRow";
 const DashboardMyAppointments = () => {
   const axiosSecure = useAxiosSecure();
   const { user } = useAuthContext();
-  const { data: myAppointments = [] } = useQuery({
+  const { data: myAppointments = [], isPending } = useQuery({
     queryKey: [user.email],
     queryFn: async () => {
       const res = await axiosSecure.get(`/my-appointments?email=${user.email}`);
@@ -16,46 +16,54 @@ const DashboardMyAppointments = () => {
   return (
     <div className=" bg-[#F1F5F9] min-h-screen p-10">
       <div>
-        <div className="pb-10">
-          <h1 className="text-3xl font-bold text-left">
-            My Appointments : {myAppointments.length}
-          </h1>
-        </div>
-        {!myAppointments.length ? (
-          <div className="hero bg-white min-h-screen">
-            <div className="hero-content text-center">
-              <div>
-                <h1 className="lg:text-5xl md:text-2xl text-xl font-bold text-slate-400">
-                  You Do Not Have <br /> Any Appointments
-                </h1>
-              </div>
-            </div>
+        {isPending ? (
+          <div className="min-h-screen flex justify-center items-center">
+            <span className="loading loading-ring loading-lg"></span>
           </div>
         ) : (
-          <div className="overflow-x-auto mt-5 rounded-lg">
-            <table className="table text-center">
-              {/* head */}
-              <thead className="bg-[#E6E6E6] text-black">
-                <tr>
-                  <th>#</th>
-                  <th>NAME</th>
-                  <th>DATE</th>
-                  <th>TIME</th>
-                  <th>TREATMENT</th>
-                  <th>PAYMENT</th>
-                </tr>
-              </thead>
-              <tbody className="bg-white">
-                {myAppointments.map((appointment, index) => (
-                  <AppointmentsTableRow
-                    key={appointment._id}
-                    index={index}
-                    appointment={appointment}
-                  />
-                ))}
-              </tbody>
-            </table>
-          </div>
+          <>
+            <div className="pb-10">
+              <h1 className="text-3xl font-bold text-left">
+                My Appointments : {myAppointments.length}
+              </h1>
+            </div>
+            {!myAppointments.length ? (
+              <div className="hero bg-white min-h-screen">
+                <div className="hero-content text-center">
+                  <div>
+                    <h1 className="lg:text-5xl md:text-2xl text-xl font-bold text-slate-400">
+                      You Do Not Have <br /> Any Appointments
+                    </h1>
+                  </div>
+                </div>
+              </div>
+            ) : (
+              <div className="overflow-x-auto mt-5 rounded-lg">
+                <table className="table text-center">
+                  {/* head */}
+                  <thead className="bg-[#E6E6E6] text-black">
+                    <tr>
+                      <th>#</th>
+                      <th>NAME</th>
+                      <th>DATE</th>
+                      <th>TIME</th>
+                      <th>TREATMENT</th>
+                      <th>PAYMENT</th>
+                    </tr>
+                  </thead>
+                  <tbody className="bg-white">
+                    {myAppointments.map((appointment, index) => (
+                      <AppointmentsTableRow
+                        key={appointment._id}
+                        index={index}
+                        appointment={appointment}
+                      />
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            )}
+          </>
         )}
       </div>
     </div>
